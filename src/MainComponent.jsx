3c911f2b1f75cb1ef84acee9825cdbe53b8e0a8a
Jamie Kudla <kudla@teralogics.com>
2019-03-26 21:25:56 -0400

    Add redux basics

--- src/MainComponent.jsx
+++ src/MainComponent.jsx
@@ -1,13 +1,21 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
 
 import GlobalStyles from './assets/styles/global';
 import App from './App/AppComponent';
 
-const MainComponent = () => (
+const MainComponent = ({ store }) => (
 	<Fragment>
 		<GlobalStyles />
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</Fragment>
 );
+
+MainComponent.propTypes = {
+	store: PropTypes.object,
+};
 
 export default MainComponent;
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import ReduxThunk from 'redux-thunk';
 
+import rootReducer from './store';
 import Main from './MainComponent';
 
+const composeEnhancers = composeWithDevTools({});
+const store = createStore(
+	rootReducer,
+	{},
+	composeEnhancers(applyMiddleware(ReduxThunk)),
+);
 const appRoot = document.getElementById('appRoot');
 
-ReactDOM.render(<Main />, appRoot);
+ReactDOM.render(<Main store={store} />, appRoot);
